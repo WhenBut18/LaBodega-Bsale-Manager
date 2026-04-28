37c3bba9b3a92a82071fe6c29f857e3ff0b67a9e
WhenBut18 <juanpablo_fuenzalida08@hotmail.com>
2026-04-28 15:05:38 -0400

    ldl

--- API/app.js
+++ API/app.js
@@ -128,7 +128,7 @@ app.get('/datos/precios', async (req, res) => {
 app.get('/datos/referencias', async (req, res) => {
   const referencia = req.headers['referencia']
   if (!referencia) {
-    res.status(500).json({ error: 'falta referencia', tuheardes: referencia })
+    res.status(500).json({ error: referencia })
   }
   try {
     const response = axios.get(bsaleURL + referencia, {
