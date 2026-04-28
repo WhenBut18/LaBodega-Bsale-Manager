a6c4335c51e97c0f9057cddf9830f6463a49097a
WhenBut18 <juanpablo_fuenzalida08@hotmail.com>
2026-04-28 14:39:40 -0400

    cambio de parametro a headers

--- API/app.js
+++ API/app.js
@@ -126,7 +126,7 @@ app.get('/datos/precios', async (req, res) => {
 })
 
 app.get('/datos/referencias', async (req, res) => {
-  const referencia = req.body.referencia
+  const referencia = req.headers.referencia
   if (!referencia) {
     res.status(500).json({ error: 'falta referencia' })
   }
